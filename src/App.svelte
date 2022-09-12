dc2e62ea03997f0dc8a46b9f69508c6166267f3b
Raicuparta <Raicuparta@users.noreply.github.com>
2022-09-12 02:18:55 +0100

    add pagination

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,13 @@
 
   let objectRecord: Record<string, string> = {};
   let filteredObjectList: [string, string][];
+  let paginatedObjectList: [string, string][];
   let filter = "";
   let filterDebounceTimer = -1;
+
+  const maxPerMage = 50;
+  let currentPage = 0;
+  let pageCount = 0;
 
   $: {
     (async () => {
@@ -18,12 +23,22 @@
     filteredObjectList = Object.entries(objectRecord).filter((entry) =>
       entry[1].toLocaleLowerCase().includes(filter.toLocaleLowerCase())
     );
+
+    pageCount = Math.ceil(filteredObjectList.length / maxPerMage);
+    currentPage = 0;
   }
 
-  const setFilterDebounce = (value) => {
+  $: {
+    paginatedObjectList = filteredObjectList.slice(
+      currentPage * maxPerMage,
+      currentPage * maxPerMage + maxPerMage
+    );
+  }
+
+  const setFilterDebounce = (newFilter: string) => {
     clearTimeout(filterDebounceTimer);
     filterDebounceTimer = setTimeout(() => {
-      filter = value;
+      filter = newFilter;
     }, 300);
   };
 </script>
@@ -34,8 +49,20 @@
     placeholder="Filter"
     on:keyup={({ currentTarget: { value } }) => setFilterDebounce(value)}
   />
+  <div class="flex flex-wrap">
+    {#each Array(pageCount) as _, pageIndex}
+      <button
+        class="bg-black p-1 m-1"
+        class:bg-white={pageIndex == currentPage}
+        class:text-black={pageIndex == currentPage}
+        on:click={() => (currentPage = pageIndex)}
+      >
+        {pageIndex}
+      </button>
+    {/each}
+  </div>
   <div class="grid grid-cols-4">
-    {#each filteredObjectList as [file, path]}
+    {#each paginatedObjectList as [file, path]}
       <GameObject {file} {path} />
     {/each}
   </div>
