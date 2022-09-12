0c6eedbd1bad467c72e206b569e1140c5cf8f635
Raicuparta <Raicuparta@users.noreply.github.com>
2022-09-12 02:26:03 +0100

    add items per page

--- src/App.svelte
+++ src/App.svelte
@@ -7,7 +7,7 @@
   let filter = "";
   let filterDebounceTimer = -1;
 
-  const maxPerMage = 50;
+  let itemsPerPage = 50;
   let currentPage = 0;
   let pageCount = 0;
 
@@ -24,14 +24,14 @@
       entry[1].toLocaleLowerCase().includes(filter.toLocaleLowerCase())
     );
 
-    pageCount = Math.ceil(filteredObjectList.length / maxPerMage);
+    pageCount = Math.ceil(filteredObjectList.length / itemsPerPage);
     currentPage = 0;
   }
 
   $: {
     paginatedObjectList = filteredObjectList.slice(
-      currentPage * maxPerMage,
-      currentPage * maxPerMage + maxPerMage
+      currentPage * itemsPerPage,
+      currentPage * itemsPerPage + itemsPerPage
     );
   }
 
@@ -41,14 +41,33 @@
       filter = newFilter;
     }, 300);
   };
+
+  const setItemsPerPageDebounce = (newItemsPerPage: number) => {
+    clearTimeout(filterDebounceTimer);
+    filterDebounceTimer = setTimeout(() => {
+      itemsPerPage = newItemsPerPage;
+    }, 300);
+  };
 </script>
 
 <main class="m-auto p-4">
-  <input
-    class="p-2 mb-2"
-    placeholder="Filter"
-    on:keyup={({ currentTarget: { value } }) => setFilterDebounce(value)}
-  />
+  <div class="flex flex-wrap gap-4 p-2">
+    <input
+      class="p-2 mb-2"
+      placeholder="Filter"
+      on:change={({ currentTarget: { value } }) => setFilterDebounce(value)}
+    />
+    <div>
+      Items per page:
+      <input
+        type="number"
+        class="w-32 p-2"
+        value={itemsPerPage}
+        on:change={({ currentTarget: { value } }) =>
+          setItemsPerPageDebounce(Number.parseInt(value))}
+      />
+    </div>
+  </div>
   <div class="flex flex-wrap">
     {#each Array(pageCount) as _, pageIndex}
       <button
